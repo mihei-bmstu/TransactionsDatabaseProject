3e87ff066035729fc7b388f9888367ba94f040f4
mikhail <mihei-bmstu@mail.ru>
2022-11-09 19:55:35 +0300

    create tables sql script

--- src/main/resources/db/migration/V1__Create_Tables.sql
+++ src/main/resources/db/migration/V1__Create_Tables.sql
@@ -18,20 +18,20 @@ COMMENT ON TABLE clients IS 'This table holds all clients';
 
 CREATE TABLE transactions (
     transaction_id          SERIAL PRIMARY KEY,
-    client_id               INT REFERENCES clients ON DELETE RESTRICT ON DELETE CASCADE,
+    client_id               INT REFERENCES clients ON DELETE RESTRICT ON UPDATE CASCADE,
     card                    BIGINT NOT NULL,
     transaction_sum         DECIMAL(6, 2) CONSTRAINT positive_transaction CHECK (transaction_sum > 0)
 );
 
 COMMENT ON TABLE transactions IS 'This table holds all transactions';
-COMEMNT ON COLUMN cars IS 'Credit card that was used for the transaction';
+COMMENT ON COLUMN transactions.card IS 'Credit card that was used for the transaction';
 
 CREATE TABLE orders (
     order_id                SERIAL PRIMARY KEY,
     client_id               INT,
     order_datetime          TIMESTAMP,
     order_sum               DECIMAL(6, 2) CONSTRAINT positive_sum CHECK (order_sum > 0),
-    transaction_id          INT REFERENCES transactions ON DELETE RESTRICT ON UPDATE CASCADE
+    transaction_id          INT REFERENCES transactions ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "client_id_fk" FOREIGN KEY (client_id) REFERENCES clients (client_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
